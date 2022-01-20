d4aa9e9a2a9296d33766f61ad51e95eaa1dc4f3b
wubingle <bingle.wu@drgou.com>
2022-01-20 15:16:23 +0800

    关系和超链接

--- RestTest/RestTest/urls.py
+++ RestTest/RestTest/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+urlpatterns = [ 
     path('admin/', admin.site.urls),
     path('', include('snippets.urls')),
+]
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]
